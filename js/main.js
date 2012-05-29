530c70a1b957f07cc0db6c2f2d85d9934eb21376
Lawrence Mandel <lmandel@gmail.com>
2012-05-29 17:16:33 -0400

    Added support for multiple instances of the same bug in the page.

--- js/main.js
+++ js/main.js
@@ -8,16 +8,29 @@ function populateTable(){
 	var table = $("#compattable");
 	for(var i = 0; i < data.length; i++){
 		metabugs.push(data[i].bug);
+		var id = createId(data[i].bug);
 		var row = $("<tr>");
 		table.append(row);
 		row.append(createTableCell(data[i].name));
 		row.append(createTableCell(data[i].url));
-		row.append(createTableCell(createBugDiv(data[i].bug)));
-		row.append(createTableCell(createDependsDiv(data[i].bug)));
+		row.append(createTableCell(createBugDiv(id)));
+		row.append(createTableCell(createDependsDiv(id)));
 		row.append(createTableCell(data[i].info));
 	}
 	getMetabugs(metabugs);
 
+}
+
+function createId(id){
+	var docId = id;
+	if($("#bug"+id).length == 1){
+		var i = 0;
+		do {
+			i++;
+			docId = id + "_" + i;
+		}while($("#bug"+docId).length == 1);
+	}
+	return docId;
 }
 
 function createTableCell(value){
@@ -33,10 +46,14 @@ function createDependsDiv(value){
 }
 
 function createBugDiv(id, name, alias, resolved){
+	nakedId = id;
+	if(typeof id == "string"){
+		nakedId = id.split('_')[0];
+	}
 	var div = $("<div>");
 	div.attr("id", "bug"+ id);
 	var link = $("<a>");
-	link.attr("href", "https://bugzilla.mozilla.org/show_bug.cgi?id="+id);
+	link.attr("href", "https://bugzilla.mozilla.org/show_bug.cgi?id="+nakedId);
 	link.attr("title", name);
 	if(resolved){
 		link.attr("style", "text-decoration: line-through;");
@@ -45,7 +62,7 @@ function createBugDiv(id, name, alias, resolved){
 		link.append(alias);
 	}
 	else{
-		link.append(id);
+		link.append(nakedId);
 	}
 	div.append(link);
 	return div;
@@ -78,25 +95,31 @@ function getMetabugs(metabugs){
 function processMetabugs(bugs){
 	var depends = "";
 	for(var i = 0; i < bugs.length; i++){
+		var k = 0;
 		var id = bugs[i].id;
-		var bugdiv = $("#bug"+id);
-		var newbugdiv = createBugDiv(id, bugs[i].summary, bugs[i].alias, isResolved(bugs[i].status));
-		bugdiv.replaceWith(newbugdiv);
-		var localDepends = bugs[i].depends_on;
-		if(localDepends && localDepends != ""){
-			if(depends != ""){
-				depends += ",";
+		do {
+			k++;
+			var bugdiv = $("#bug"+id);
+			var newbugdiv = createBugDiv(id, bugs[i].summary, bugs[i].alias, isResolved(bugs[i].status));
+			bugdiv.replaceWith(newbugdiv);
+			var localDepends = bugs[i].depends_on;
+			if(localDepends && localDepends != ""){
+				if(depends != ""){
+					depends += ",";
+				}
+				depends += localDepends;
 			}
-			depends += localDepends;
-		}
-		var dependsdiv = $("#bug"+ id + "-depends");
-		var dependsArray = depends.split(',');
-		for(var j = 0; j < dependsArray.length; j++){
-			dependsdiv.append(createBugDiv(dependsArray[j]));
-			if(j+1 < dependsArray.length){
-				dependsdiv.append(",");
+			var dependsdiv = $("#bug"+ id + "-depends");
+			//var dependsArray = localDepends.split(',');
+			for(var j = 0; j < localDepends.length; j++){
+				dependsdiv.append(createBugDiv(createId(localDepends[j])));
+				if(j+1 < localDepends.length){
+					dependsdiv.append(",");
+				}
 			}
-		}
+			
+			id = bugs[i].id + "_" + k;
+		}while($("#bug"+id).length == 1);
 	}
 	getDependentBugs(depends);
 }
@@ -127,8 +150,13 @@ function getDependentBugs(dependentBugs){
 function processDependentbugs(bugs){
 	for(var i = 0; i < bugs.length; i++){
 		var id = bugs[i].id;
-		var bugdiv = $("#bug"+id);
-		var newbugdiv = createBugDiv(id, bugs[i].summary, bugs[i].alias, isResolved(bugs[i].status));
-		bugdiv.replaceWith(newbugdiv);
+		var k = 0;
+		do{
+			k++;
+			var bugdiv = $("#bug"+id);
+			var newbugdiv = createBugDiv(id, bugs[i].summary, bugs[i].alias, isResolved(bugs[i].status));
+			bugdiv.replaceWith(newbugdiv);
+			id = bugs[i].id + "_" + k;
+		}while($("#bug"+id).length == 1);
 	}
 }
