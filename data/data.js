f862dc9347f7a877404bc6d1ebcc7fb0212eb428
Lawrence Mandel <lmandel@gmail.com>
2012-06-11 11:51:25 -0400

    Added remaining top 50 Alexa Brazilian sites.

--- data/data.js
+++ data/data.js
@@ -67,7 +67,7 @@ var data = [{
     "url": "terra.com.br", 
     "bug": 0, 
     "info": ""
-  }/*,	
+  },	
   {
     "name": "Twitter", 
     "company": "Twitter",
@@ -107,7 +107,7 @@ var data = [{
     "name": "MSN", 
     "company": "Microsoft",
     "url": "msn.com", 
-    "bug": 759122, 
+    "bug": -1, 
     "info": ""
   },
   {
@@ -135,6 +135,216 @@ var data = [{
     "name": "Orkut", 
     "company": "Google",
     "url": "orkut.com", 
-    "bug": 668288, 
+    "bug": -1, 
     "info": ""
-  }*/]
+  },
+   {
+    "name": "LinkedIn", 
+    "company": "LinkedIn",
+    "url": "linkedin.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Babylon", 
+    "company": "Babylon",
+    "url": "babylon.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "googleusercontent.com", 
+    "company": "googleusercontent.com",
+    "url": "googleusercontent.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "t.co", 
+    "company": "t.co",
+    "url": "t.co", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Folha OnLine", 
+    "company": "Folha OnLine",
+    "url": "folha.uol.com.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "4shared", 
+    "company": "4shared",
+    "url": "4shared.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Blogger.com", 
+    "company": "Google",
+    "url": "blogger.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "xvideos.com", 
+    "company": "xvideos.com",
+    "url": "xvideos.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "r7.com", 
+    "company": "r7.com",
+    "url": "r7.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Tumblr", 
+    "company": "Tumblr",
+    "url": "tumblr.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Blogspot.com", 
+    "company": "Google",
+    "url": "blogspot.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Caixa Economica Federal", 
+    "company": "Caixa Exonomica Federal",
+    "url": "caixa.gov.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Microsoft Corporation", 
+    "company": "Microsoft",
+    "url": "microsoft.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Uol Esporte", 
+    "company": "Uol Esporte",
+    "url": "esporte.uol.com.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Correios", 
+    "company": "Correios",
+    "url": "correios.com.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Bb", 
+    "company": "Bb",
+    "url": "bb.com.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Ministerio da Fazenda", 
+    "company": "Ministerio da Fazenda",
+    "url": "fazenda.gov.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "BuscaPe", 
+    "company": "BuscaPe",
+    "url": "buscape.com.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Diretorio Estadao", 
+    "company": "Diretorio Estadao",
+    "url": "estadao.com.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Banco Bradesco S/A", 
+    "company": "Banco Bradesco S/A",
+    "url": "bradesco.com.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Ask", 
+    "company": "Ask",
+    "url": "ask.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "noticias.uol.com.br", 
+    "company": "noticias.uol.com.br",
+    "url": "noticias.uol.com.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Banco do Brasil", 
+    "company": "Banco do Brasil",
+    "url": "bancobrasil.com.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "sp.gov.br", 
+    "company": "sp.gov.br",
+    "url": "sp.gov.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Americanas.com", 
+    "company": "Americanas.com",
+    "url": "americanas.com.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Submarino", 
+    "company": "Submarino",
+    "url": "submarino.com.br", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "bp.blogspot.com", 
+    "company": "bp.blogspot.com",
+    "url": "bp.blogspot.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Amazon", 
+    "company": "Amazon",
+    "url": "amazon.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Flickr", 
+    "company": "Yahoo",
+    "url": "flickr.com", 
+    "bug": -1, 
+    "info": ""
+  },
+   {
+    "name": "Oi", 
+    "company": "Oi",
+    "url": "oi.com.br", 
+    "bug": -1, 
+    "info": ""
+  }]
