49a5410124c118e71230f7250d1dec9683d79180
Lawrence Mandel <lmandel@gmail.com>
2012-06-12 23:35:01 -0400

    Created top lists for Germany, Spain, France, Italy, Japan, South Korea, and the United Kingdom.

--- data/toplists.js
+++ data/toplists.js
@@ -4,7 +4,42 @@ var topLists = [{
 	 "id": "alexa50us"
 	},
 	{
-	 "name": "Alexa 50 Brazil",
+	 "name": "Alexa 50 BR",
 	 "url": "alexa_br.json",
 	 "id": "alexa50br"
+	},
+	{
+	 "name": "Alexa 50 DE",
+	 "url": "alexa_de.json",
+	 "id": "alexa50de"
+	},
+	{
+	 "name": "Alexa 50 UK",
+	 "url": "alexa_uk.json",
+	 "id": "alexa50uk"
+	},
+	{
+	 "name": "Alexa 50 ES",
+	 "url": "alexa_es.json",
+	 "id": "alexa50es"
+	},
+	{
+	 "name": "Alexa 50 FR",
+	 "url": "alexa_fr.json",
+	 "id": "alexa50fr"
+	},
+	{
+	 "name": "Alexa 50 IT",
+	 "url": "alexa_it.json",
+	 "id": "alexa50it"
+	},
+	{
+	 "name": "Alexa 50 JP",
+	 "url": "alexa_jp.json",
+	 "id": "alexa50jp"
+	},
+	{
+	 "name": "Alexa 50 KR",
+	 "url": "alexa_kr.json",
+	 "id": "alexa50kr"
 	}];
