f768d041606e9a4a251203d99cb300bb5876d6ce
Lawrence Mandel <lmandel@gmail.com>
2012-06-19 01:24:50 -0400

    Added business developer top sites list. Shortened names of Alexa top 50 lists and ordered alphabetically.

--- data/toplists.js
+++ data/toplists.js
@@ -1,45 +1,50 @@
 var topLists = [{
-	 "name": "Alexa 50 US",
-	 "url": "alexa_us.json",
-	 "id": "alexa50us"
+	 "name": "BD Top Sites",
+	 "url": "bdtopsites.json",
+	 "id": "bdtopsites"
 	},
 	{
-	 "name": "Alexa 50 BR",
+	 "name": "BR 50",
 	 "url": "alexa_br.json",
 	 "id": "alexa50br"
 	},
 	{
-	 "name": "Alexa 50 DE",
+	 "name": "DE 50",
 	 "url": "alexa_de.json",
 	 "id": "alexa50de"
 	},
 	{
-	 "name": "Alexa 50 UK",
-	 "url": "alexa_uk.json",
-	 "id": "alexa50uk"
-	},
-	{
-	 "name": "Alexa 50 ES",
+	 "name": "ES 50",
 	 "url": "alexa_es.json",
 	 "id": "alexa50es"
 	},
 	{
-	 "name": "Alexa 50 FR",
+	 "name": "FR 50",
 	 "url": "alexa_fr.json",
 	 "id": "alexa50fr"
 	},
 	{
-	 "name": "Alexa 50 IT",
+	 "name": "IT 50",
 	 "url": "alexa_it.json",
 	 "id": "alexa50it"
 	},
 	{
-	 "name": "Alexa 50 JP",
+	 "name": "JP 50",
 	 "url": "alexa_jp.json",
 	 "id": "alexa50jp"
 	},
 	{
-	 "name": "Alexa 50 KR",
+	 "name": "KR 50",
 	 "url": "alexa_kr.json",
 	 "id": "alexa50kr"
+	},
+	{
+	 "name": "UK 50",
+	 "url": "alexa_uk.json",
+	 "id": "alexa50uk"
+	},
+	{
+	 "name": "US 50",
+	 "url": "alexa_us.json",
+	 "id": "alexa50us"
 	}];
