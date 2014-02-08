91a72fe336fd8c6f99523e5aa26234cb11bfc0a2
Hallvord R. M. Steen <hsteen@mozilla.com>
2014-02-07 20:25:37 -0800

    bugfix for timelines.js: file names and list ids have changed

--- js/timelines.js
+++ js/timelines.js
@@ -258,9 +258,9 @@ function showListDetails(newHash, excludeUS, showSitesWithoutBugs){
 function shouldExcludeUSSite(list, host){
 	// now.. the local lists become more interesting and useful if the big, "global" sites are taken out.
 	// So let's skip sites from local listings even if they are popular in that country..
-	if(list === 'alexa1000' || list === 'alexa50us') return false; // include all sites in these lists
+	if(list === '1000' || list === 'us50') return false; // include all sites in these lists
 	//if( masterBugTable.lists['alexa1000'] && masterBugTable.lists['alexa1000'].data.indexOf(host)>-1  && masterBugTable.lists['alexa1000'].data.indexOf(host)<alexaGlobalTreshold ){
-	if( list !== 'alexa50us' && masterBugTable.lists['alexa50us'] && masterBugTable.lists['alexa50us'].data.indexOf(host)>-1 /* && masterBugTable.lists['alexa1000'].data.indexOf(host)<alexaGlobalTreshold */){
+	if( list !== 'us50' && masterBugTable.lists['us50'] && masterBugTable.lists['us50'].data.indexOf(host)>-1 /* && masterBugTable.lists['alexa1000'].data.indexOf(host)<alexaGlobalTreshold */){
 		return true;
 	}
 	return false;
